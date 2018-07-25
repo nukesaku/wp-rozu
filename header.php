743497bf8a92b2a2772065de79aec4def236f1b3
rozu <nextniconico@gmail.com>
2018-07-26 00:02:33 +0900

    [Add]file add

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+
+  <?php wp_head() ?>
+</head>
+<body <?php body_class(); ?>>
+  <header>
+    
+  </header>
