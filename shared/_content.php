08f076b610bd20a37e55e15b10c093be10ca8f8f
rozu <nextniconico@gmail.com>
2018-09-12 00:38:05 +0900

    日付・サムネイル・抜粋

--- shared/_content.php
+++ shared/_content.php
@@ -1,3 +1,11 @@
-<a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-<?php the_post_thumbnail() ?>
+
+<?php if (is_singular()) : ?>
+
+  <?php the_content() ?>
+<?php else : ?>
+  <?php the_post_thumbnail('thumbnail') ?>
+  <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+  <?php the_time('Y年n月j日') ?>
+  <?php the_excerpt(); ?>
+<?php endif; ?>
 <hr>
