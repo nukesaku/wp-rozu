29bec88b03037c6887a7c60f3ea67dd51cad071b
rozu <nextniconico@gmail.com>
2019-01-08 16:38:39 +0900

    babel update

--- js/sample.js
+++ js/sample.js
@@ -4,3 +4,11 @@ var square = function square(x) {
   return x * x;
 };
 console.log("result =" + square(5));
+
+const map = new Map([
+  [1, 'element1'],
+  [2, 'element2'],
+  [3, 'element3'],
+]);
+
+console.log(map);
